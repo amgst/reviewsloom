5a44f96937bbda87067f60f730d0ade4243ac84d
amgst <amgst99@gmail.com>
2026-09-19 12:57:12 +0500

    k

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -18,6 +18,7 @@ export default function App() {
 
   return (
     <AppProvider isEmbeddedApp apiKey={apiKey}>
+      <script src="https://cdn.shopify.com/shopifycloud/polaris-1.js" />
       <NavMenu>
         <Link to="/app" rel="home">
           Reviews
